d3d4a1c7e3d19a522a047e3e653f860a1a356be7
Andrew Pontious <apontious@gmail.com>
2014-11-26 15:07:22 -0800

    Switch to Swift, part 1: change all auto-generated, untouched files.
    Files copied from a second project, made with Swift from the beginning, to be sure I do things the "Apple" way.

---
+++ Watch MatchTests/Watch_MatchTests.swift
@@ -0,0 +1,36 @@
+//
+//  Watch_MatchTests.swift
+//  Watch MatchTests
+//
+//  Created by Andrew Pontious on 11/23/14.
+//  Copyright (c) 2014 TeamHand. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class Watch_MatchTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
